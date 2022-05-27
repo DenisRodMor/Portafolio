ba016d57ecd05e9f5ffa8b977c831bb719dc5537
DenisRodMor <104526631+DenisRodMor@users.noreply.github.com>
2022-05-27 08:21:44 -0600

    Añadiendo Hasha las rutas, modificando footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,15 @@ import { ItemComponent } from "./pages/item/item.component";
 import { PortafolioComponent } from "./pages/portafolio/portafolio.component";
 
 const app_routes : Routes = [
-  {path: '', component: PortafolioComponent},
+  {path: 'home', component: PortafolioComponent},
   {path: 'about', component: AboutComponent},
   {path: 'item', component: ItemComponent},
-  {path: '**', pathMatch:'full' ,redirectTo: '' }
+  {path: '**', pathMatch:'full' ,redirectTo: 'home' }
 
 ];
 
 @NgModule({
-  imports:[RouterModule.forRoot(app_routes)],
+  imports:[RouterModule.forRoot(app_routes, {useHash: true})], //funciones hash son estructuras de datos de uso común en los sistemas informáticos para tareas, como verificar la integridad de los mensajes y autenticar la información.
   exports: [RouterModule]
 
 })
